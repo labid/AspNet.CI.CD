4b5da5cd198c0716b6c445c4ac61a2cb6c3d0a37
labid <labid.imad@gmail.com>
2022-10-10 19:42:32 +0200

    Ajout test unitaire

---
+++ AspNet.CI.CD.github/TestUnitaire.CI.CD.github/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace TestUnitaire.CI.CD.github
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
