ddf96d4d372dcbcca5525dfb502c515c38ffd616
labid <labid.imad@gmail.com>
2022-10-10 19:37:53 +0200

    Version 0.1 initialisation de projet

---
+++ AspNet.CI.CD.github/AspNet.CI.CD.github/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspNet.CI.CD.github.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
